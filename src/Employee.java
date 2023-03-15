7cad6457e61db550998cbc6cdadb4d0b024d39a4
vadim <vadimpoldolin19@gmail.com>
2023-03-15 20:20:13 +0100

    Почти конец

---
+++ src/Employee.java
@@ -0,0 +1,53 @@
+public class Employee {
+    private String fullName;
+    private int salary;
+    private static int personIdInc = 0;
+    private int personId;
+    private int departmentId;
+
+    public Employee(String fullName, int salary, int departmentId) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.departmentId = departmentId;
+        this.personId = personIdInc++;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getDepartmentId() {
+        return this.departmentId;
+    }
+
+    public void setFullName() {
+        this.fullName = fullName;
+    }
+
+    public void setSalary() {
+        this.salary = salary;
+    }
+
+    public void setDepartmentId() {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee {" +
+                "personId = " + personId +
+                ", fullName = " + fullName  +
+                ", salary = " + salary +
+                ", departmentId = " + departmentId +
+                '}';
+    }
+
+    public int getPersonId() {
+        return this.personId;
+    }
+}
+
